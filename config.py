6859be6e90896dee9e2d3ba03a9f987e057ab29c
Namgay Choden <164040031+Nchoden@users.noreply.github.com>
2025-05-14 21:54:05 +0800

    small edit for test

--- config.py
+++ config.py
@@ -7,3 +7,8 @@ class Config:
     SECRET_KEY = "replace-with-random-string"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "sf6_spotlight.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    WTF_CSRF_ENABLED = False
