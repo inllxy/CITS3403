cae559f4e59174a7b18478a2f7add94a10b56f0c
Harper Chen <harper020805@gmail.com>
2025-05-16 23:29:19 +0800

    fix config

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # run.py
 from app import create_app
+from config import Config
 
-app = create_app()
+app = create_app(Config)
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
